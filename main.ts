f03ba215b57837a536e4dfb8eedc6c3be71891a1
ymc9 <yiming@whimslab.io>
2025-08-12 19:46:41 +0800

    update

--- main.ts
+++ main.ts
@@ -3,6 +3,32 @@ import { createClient } from './db';
 async function main() {
   const db = await createClient();
 
+  console.log('Create two users with a sequential transaction');
+  const users = await db.$transaction([
+    db.user.create({ data: { email: 'u1@test.com' } }),
+    db.user.create({ data: { email: 'u2@test.com' } }),
+  ]);
+  console.log(users);
+
+  console.log('Create two users with unique constraint violation');
+  try {
+    await db.$transaction([
+      db.user.create({ data: { email: 'u3@test.com' } }),
+      db.user.create({ data: { email: 'u3@test.com' } }),
+      ]
+    );
+  } catch (err: any) {
+    console.log('Transaction rolled back due to:', err.cause.message);
+    console.log('User created:', await db.user.findUnique({where: { email: 'u3@test.com' } }));
+  }
+
+  console.log('Create user and post with an interactive transaction');
+  const [user, post] = await db.$transaction(async (tx) => {
+    const user = await tx.user.create({ data: { email: 'u3@test.com' } });
+    const post = await tx.post.create({ data: { title: 'Post1', authorId: user.id } });
+    return [user, post];
+  });
+  console.log('Created user and post:', user, post);
 }
 
 main();
